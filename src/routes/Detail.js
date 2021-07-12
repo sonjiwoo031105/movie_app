d0c14796fc5f35cd2ef24130d589522732caab6f
Jiwoo Son <jiji432@naver.com>
2021-07-12 19:05:52 +0900

    #6.4 Redirecting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-function Detail(){
-    return(
-        <div>
-            <h1>Hello</h1>
-        </div>
-    );
+class Detail extends React.Component{
+    componentDidMount(){
+        const { location, history } = this.props;
+        if(location.state === undefined){
+            history.push("/");
+        }
+    }
+    render(){
+        const { location } = this.props;
+        if (location.state) {
+            return <span>{location.state.summary}</span>;
+        } else {
+            return null;
+        }
+    }
 }
 export default Detail;
