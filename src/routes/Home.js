d67926f978b7d7a74215845841810916877fca23
Jiwoo Son <jiji432@naver.com>
2021-07-13 22:36:11 +0900

    fixed css

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 import Movie from "../components/Movie";
 import "./Home.css";
 
@@ -8,20 +8,20 @@ class Home extends React.Component {
     isLoading: true,
     movies: []
   };
-
   getMovies = async () => {
     const {
       data: {
-        data: {movies}
+        data: { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading: false})
-  }
-  componentDidMount(){
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
     this.getMovies();
   }
-
-  render(){
+  render() {
     const { isLoading, movies } = this.state;
     return (
       <section className="container">
@@ -32,19 +32,19 @@ class Home extends React.Component {
         ) : (
           <div className="movies">
             {movies.map(movie => (
-            <Movie 
-              key={movie.id} 
-              id={movie.id} 
-              year={movie.year} 
-              title={movie.title} 
-              summary={movie.summary} 
-              poster={movie.medium_cover_image} 
-              genres={movie.genres}
-            />
-          ))}
-          </div>    
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
         )}
-    </section>
+      </section>
     );
   }
 }
